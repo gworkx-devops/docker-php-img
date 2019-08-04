83852c9a367424c50471226d866471923f7a9466
isimon-glu <irin.simon@glu.com>
2019-08-04 17:51:23 -0400

    fix for \'Unable to locate package php7.3-mcrypt\'

--- Dockerfile.debian.php
+++ Dockerfile.debian.php
@@ -28,7 +28,7 @@ RUN set -x \
     php7.3-mbstring php7.3-intl php7.3-geoip php7.3-apcu php7.3-memcached php7.3-opcache \
     php7.3-imap php7.3-bz2 php7.3-ldap php7.3-readline php7.3-redis php7.3-ssh2 php7.3-soap \
     php7.3-phar php7.3-xml php7.3-xmlrpc php7.3-xsl \
-    php7.3-imagick php7.3-gd php7.3-mcrypt php7.3-json php7.3-curl php7.3-zip
+    php7.3-imagick php7.3-gd php7.3-json php7.3-curl php7.3-zip
 
 # clean up APT when done
 #
